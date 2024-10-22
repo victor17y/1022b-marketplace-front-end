cb04800ea0c1ddfa5072fd08aa3683ecfe224792
Victor Hugo <118356177+victor17y@users.noreply.github.com>
2024-10-22 07:40:38 -0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-type ProdutoType={
-  id:number
-  nome:string
-  descricao:string
-  preco:string
-  imagem:string
+
+type ProdutoType = {
+  id: number,
+  nome: string,
+  descricao: string,
+  preco: string,
+  imagem: string
 }
+
+type UsuarioType = {
+  id: number,
+  nome: string,
+  email: string
+}
+
 function App() {
-  const[produtos,setprodutos]=useState<ProdutoType[]>([])
-useEffect(()=>{
-  fetch("https://one022b-marketplace-qybh.onrender.com/produtos")
-  .then(resposta=>resposta.json())
-  .then(dados=>setprodutos(dados))
-  
-},[])
-  return (//jsx
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([])
+
+  useEffect(() => {
+    fetch("https://one022b-marketplace-qybh.onrender.com/produto")
+      .then(resposta => resposta.json())
+      .then(dados => setProdutos(dados))
+  }, [])
+
+  useEffect(() => {
+    fetch("https://one022b-marketplace-qybh.onrender.com/usuarios")
+      .then(resposta => resposta.json())
+      .then(dados => setUsuarios(dados))
+  }, [])
+
+  return (
     <>
       <div className="container-produtos">
-      {produtos.map(prod=>{
-        return(
-          <div className='produto-item'>
-            <h1>{prod.nome}</h1>
-            <p>{prod.imagem}</p>
-            <p>{prod.preco}</p>
-            <p>{prod.descricao}</p>
-          </div>
-
-        )
-
-      })}
+        {produtos.map(prod => {
+          return (
+            <div key={prod.id} className="produto-item">
+              <h1>{prod.nome}</h1>
+              <img src={prod.imagem} alt="Imagem de produto" />
+              <p>{prod.preco}</p>
+              <p>{prod.descricao}</p>
+            </div>
+          )
+        })}
+      </div>
+      <div className="container-usuarios">
+        {usuarios.map(usuario => {
+          return (
+            <div key={usuario.id} className="usuario-item">
+              <h1>{usuario.nome}</h1>
+              <p>{usuario.email}</p>
+            </div>
+          )
+        })}
       </div>
     </>
   )
 }
+
 export default App
